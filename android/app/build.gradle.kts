ca9ba1c7ded309083a65934a8343c505770660c5
Teo986L <teodoro12lucamba@gmail.com>
2026-03-14 05:24:04 +0100

    Desabilita data binding explicitamente

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.traderbot.app"
-        minSdk = 29 // Android 10
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
@@ -42,6 +42,7 @@ android {
     
     buildFeatures {
         viewBinding = true
+        dataBinding = false
     }
 }
 
